279a48d1b0ea1d8e7fa4213c02bc388f0f665c44
PedroSTVeras <39079517+PedroSTVeras@users.noreply.github.com>
2018-10-26 10:55:02 -0300

    List

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -6,12 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['about.page.scss']
 })
 export class AboutPage {
+  
+  items = [
+    'Pokémon Yellow',
+    'Super Metroid',
+    'Mega Man X',
+    'The Legend of Zelda',
+    'Pac-Man',
+    'Super Mario World',
+    'Street Fighter II',
+    'Half Life',
+    'Final Fantasy VII',
+    'Star Fox',
+    'Tetris',
+    'Donkey Kong III',
+    'GoldenEye 007',
+    'Doom',
+    'Fallout',
+    'GTA',
+    'Halo'
+  ];
 
-  numOfNotes: number;
-
-  constructor(){
-    this.numOfNotes = 5;
+  itemSelected(item: string) {
+    console.log("Selected Item", item);
   }
-
 
 }
