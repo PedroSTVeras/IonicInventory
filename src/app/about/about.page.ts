bbd708aebd6a0a64b2abf09a8874ebe2d759099a
PedroSTVeras <39079517+PedroSTVeras@users.noreply.github.com>
2018-11-07 11:10:39 -0200

    joijuo

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,34 +1,27 @@
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'app-about',
   templateUrl: 'about.page.html',
   styleUrls: ['about.page.scss']
 })
+
 export class AboutPage {
 
   items = [
-    'Pokémon Yellow\n',
-    'Super Metroid',
-    'Mega Man X',
-    'The Legend of Zelda',
-    'Pac-Man',
-    'Super Mario World',
-    'Street Fighter II',
-    'Half Life',
-    'Final Fantasy VII',
-    'Star Fox',
-    'Tetris',
-    'Donkey Kong III',
-    'GoldenEye 007',
-    'Doom',
-    'Fallout',
-    'GTA',
-    'Halo'
+
   ];
+
+  constructor(private navCtrl: NavController) {
+  }
 
   itemSelected(item: string) {
     console.log("Selected Item", item);
+  }
+
+  newNote(){
+    this.navCtrl.parent.select(0);
   }
 
 }
